fb0b7b77683d5f05223c13a7eaefa5729701077d
ManuGuerinel <manuguerienl2508@gmail.com>
2025-03-20 11:08:43 +0100

    ServoMoteur pince

--- Servo/Servo.ino
+++ Servo/Servo.ino
@@ -2,32 +2,40 @@
 
 Servo ServoPince;
 
+// PIN I/O
 int pinFeedback = A0;
 int pinConsigne = 2;
-int pos_ferme = 0;
-int pos_pince = 0;
-int pos_pince_deg = 0;
-int pos_mm = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   ServoPince.attach(pinConsigne);
-  pos_ferme = analogRead(pinFeedback);
-  pos_ferme = map(pos_ferme,97,431,0,180);
-  Serial.println(pos_ferme);
 }
 
 void loop() {
-  Serial.println(180);
-
-  pos_mm = 180 - map(0,0,32,0,180);
-  ServoPince.write(pos_mm);
-  //pos_pince = analogRead(pinFeedback);
-  //pos_pince_deg = map(pos_pince,97,431,0,180);
+  Serial.println("ouverture (de 16mm)");
+  ouverture_pince(32);
+  int pos_feedback = read_pos();
+  Serial.println(pos_feedback);
   delay(1000);
+}
 
-  pos_mm = 180 - map(32,0,32,0,180);
-  ServoPince.write(pos_mm);
-  delay(1000);
+void ouverture_pince(int ouv_mm){
+  //Ouvre la pince au mm souhaité
+  //Sature la variable ouv_mm si elle est <0 ou >32
+  if (ouv_mm > 32){
+    ouv_mm = 32;
+  }
+  if (ouv_mm < 0){
+    ouv_mm = 0;
+  }
+  int pos_deg = 180 - map(ouv_mm,0,32,0,180);
+  ServoPince.write(pos_deg);
+}
+
+int read_pos(){
+  //retourne la valeur en mm de l'ouverture de la pince (0 fermé, 32 ouvert)
+  int pos = analogRead(pinFeedback);
+  pos = map(pos,97,431,32,0);
+  return pos;
 }
