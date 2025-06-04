0c6193a7ccdb777a40367ce68d144661c85cf536
Maxence-Santos <86566869+Maxence-Santos@users.noreply.github.com>
2025-06-04 11:44:34 +0200

    Update recherche_balise.h

--- Projet/recherche_balise.h
+++ Projet/recherche_balise.h
@@ -7,6 +7,8 @@
 
 #define Broche_Echo_1 5 // Broche Echo du HC-SR04 sur D7 // 
 #define Broche_Trigger_1 4 // Broche Trigger du HC-SR04 sur D8 //
+#define Broche_Echo_2 8 // Broche Echo du HC-SR04 sur D7 // 
+#define Broche_Trigger_2 10 // Broche Trigger du HC-SR04 sur D8 //
 
 void start() ;
 
